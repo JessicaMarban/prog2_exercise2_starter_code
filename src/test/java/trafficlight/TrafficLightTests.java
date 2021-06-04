18b87e45275862cdbac608afb6f026584a008fd2
JessicaMarban <jessica.marban@stud.fh-campuswien.ac.at>
2021-06-04 11:11:11 +0200

    next few test

--- src/test/java/trafficlight/TrafficLightTests.java
+++ src/test/java/trafficlight/TrafficLightTests.java
@@ -20,11 +20,96 @@ public class TrafficLightTests {
     @DisplayName("check Singleton")
     public void SingletonTest2() {
         //we compare the current state at the beginning with green state that it should start with
+        assertNotEquals(TrafficLightCtrl.getInstance().getCurrentState(), TrafficLightCtrl.getInstance().getYellowState());
+    }
+
+    @Test
+    @DisplayName("check Singleton")
+    public void SingletonTest3() {
+        //we compare the current state at the beginning with green state that it should start with
         assertNotEquals(TrafficLightCtrl.getInstance().getCurrentState(), TrafficLightCtrl.getInstance().getRedState());
     }
 
+    @Test
+    @DisplayName("check green to yellow")
+    public void ChangeFromGreenToYellow() {
+        TrafficLightCtrl control = TrafficLightCtrl.getInstance();
+        control.nextState();
+        assertEquals(control.getCurrentState(), control.getYellowState());
+    }
 
+    @Test
+    @DisplayName("check green to yellow")
+    public void ChangeFromGreenToYellow2() {
+        TrafficLightCtrl control = TrafficLightCtrl.getInstance();
+        control.nextState();
+        assertNotEquals(control.getCurrentState(), control.getGreenState());
+    }
 
+    @Test
+    @DisplayName("check green to yellow")
+    public void ChangeFromGreenToYellow3() {
+        TrafficLightCtrl control = TrafficLightCtrl.getInstance();
+        control.nextState();
+        assertNotEquals(control.getCurrentState(), control.getRedState());
+    }
+
+    @Test
+    @DisplayName("check yellow to red")
+    public void ChangeFromYellowtoRed() {
+        TrafficLightCtrl control = TrafficLightCtrl.getInstance();
+        control.nextState();
+        control.nextState();
+        assertEquals(control.getCurrentState(), control.getRedState());
+    }
+
+    @Test
+    @DisplayName("check yellow to red")
+    public void ChangeFromYellowtoRed2() {
+        TrafficLightCtrl control = TrafficLightCtrl.getInstance();
+        control.nextState();
+        control.nextState();
+        assertNotEquals(control.getCurrentState(), control.getYellowState());
+    }
+
+    @Test
+    @DisplayName("check yellow to red")
+    public void ChangeFromYellowtoRed3() {
+        TrafficLightCtrl control = TrafficLightCtrl.getInstance();
+        control.nextState();
+        control.nextState();
+        assertNotEquals(control.getCurrentState(), control.getGreenState());
+    }
+
+    @Test
+    @DisplayName("check red to yellow")
+    public void ChangeFromRedToYellow() {
+        TrafficLightCtrl control = TrafficLightCtrl.getInstance();
+        control.nextState();
+        control.nextState();
+        control.nextState();
+        assertEquals(control.getCurrentState(), control.getYellowState());
+    }
+
+    @Test
+    @DisplayName("check red to yellow")
+    public void ChangeFromRedToYellow2() {
+        TrafficLightCtrl control = TrafficLightCtrl.getInstance();
+        control.nextState();
+        control.nextState();
+        control.nextState();
+        assertNotEquals(control.getCurrentState(), control.getRedState());
+    }
+
+    @Test
+    @DisplayName("check red to yellow")
+    public void ChangeFromRedToYellow3() {
+        TrafficLightCtrl control = TrafficLightCtrl.getInstance();
+        control.nextState();
+        control.nextState();
+        control.nextState();
+        assertNotEquals(control.getCurrentState(), control.getGreenState());
+    }
 
 
 }
